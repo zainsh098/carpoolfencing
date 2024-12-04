5a0d5276215a6ab8ac6ba88d190877fbc410d289
Zain Shakoor <92949137+zainsh098@users.noreply.github.com>
2024-12-04 17:38:28 +0500

    GeocodingViewModel

--- app/src/main/java/com/example/carpoolfencing/viewmodel/GecodingViewModel.kt
+++ app/src/main/java/com/example/carpoolfencing/viewmodel/GecodingViewModel.kt
@@ -4,34 +4,44 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.carpoolfencing.network.RetrofitInstance
+import com.example.carpoolfencing.repository.SharedRepository
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
-class GeocodingViewModel(
+class GeocodingViewModel : ViewModel() {
 
-) : ViewModel() {
+    private val _coordinatesFetched = MutableStateFlow(false)
+    val coordinatesFetched: StateFlow<Boolean> = _coordinatesFetched
 
-    fun fetchCoordinates(query: String) {
+    fun fetchCoordinates(queryStart: String, queryEnd: String) {
         viewModelScope.launch {
+            try {
+                val startResponse = RetrofitInstance.geoCodingApi.getGeocode(
+                    queryStart,
+                    apiKey = "231bwmiYI6NZNOod9nAxYHmfPRGP5ssn"
+                )
+                val startCoordinates = LatLng(
+                    startResponse.results[0].position.lat,
+                    startResponse.results[0].position.lon
+                )
+                SharedRepository.setStartCoordinates(startCoordinates)
 
-            val response = RetrofitInstance.geoCodingApi.getGeocode(
-                query,
-                apiKey = "231bwmiYI6NZNOod9nAxYHmfPRGP5ssn"
-            )
+                val endResponse = RetrofitInstance.geoCodingApi.getGeocode(
+                    queryEnd,
+                    apiKey = "231bwmiYI6NZNOod9nAxYHmfPRGP5ssn"
+                )
+                val endCoordinates = LatLng(
+                    endResponse.results[0].position.lat,
+                    endResponse.results[0].position.lon
+                )
+                SharedRepository.setEndCoordinates(endCoordinates)
 
-            val lat = response.results[0].position.lat
-            val long = response.results[0].position.lon
-
-
-
-
-
-            Log.d("Lat & Long ", "${lat} ${long}}")
-
-            Log.d("API REPOSNE ", response.toString())
+                _coordinatesFetched.value = true
+            } catch (e: Exception) {
+                Log.e("Geocoding", "Error fetching coordinates: ${e.message}")
+            }
         }
-
-
     }
-
-
 }
